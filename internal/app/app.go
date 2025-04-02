2fbe9c7c5bb66e9955c9faad0c1a4addbd67e22c
Amirtaha Javadi <amirtahajavadidev@gmail.com>
2025-04-02 22:50:56 +0330

    routes and go-chi added

--- internal/app/app.go
+++ internal/app/app.go
@@ -14,4 +14,5 @@ func CreateNewApp() (app *Application, err error) {
 	return &Application{
 		Logger: logger,
 	}, nil
+
 }
--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/amirtahajavadi/httpserver/internal/app"
 )
 
 func main() {
+	port := flag.Int("port", 5000, "Server port")
+	flag.Parse()
 	app, err := app.CreateNewApp()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("hello world", app.Logger)
+	app.Logger.Println("app is running")
 
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", *port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	http.HandleFunc("/health", HealthChecker)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		return
+	}
+
+}
+
+func HealthChecker(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "status is available\n")
 }
